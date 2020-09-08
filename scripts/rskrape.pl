fa564fb9765509906d857426f064b8f8c7c885af
farlies <farlies@gmail.com>
2020-09-08 12:22:52 -0500

    skrape announcements too (most of them)

--- scripts/rskrape.pl
+++ scripts/rskrape.pl
@@ -1,16 +1,28 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
-# This script will produce a JSON summary of audio resources in
-# the designated music directory (default: $HOME/Music). This
-# resource is used by the rcal web configurator. The assumed
-# structure of this directory is:  Artist/Album/Track
-# It will also catalog the playlists (in PlaylistDir/)
+# This script will produce a JSON summary of audio resources in the
+# designated music directory (default: $HOME/Music). This resource is
+# used by the rcal web configurator. The assumed structure of this
+# directory is: Artist/Album/Track. If your music library is elsewhere,
+# specify its path in the environment variable MUSICDIR, e.g.
+#       $ MUSICDIR=/opt/music rskrape.pl > catalog.json
+#
+# Rskrape will also catalog the playlists (in PlaylistDir/)
+# The playlist are assumed to reside in the PlaylistDir,
+# by default ~/.config/mpd/playlists; override with environment
+# variable PLAYLISTDIR.
+#
+# Finally, rskrape will include a catalog of announcements if available.
+# It looks in the directories ~/.config/rsked/{resources,motd} and
+# produces rsked source specifications based on contents and some
+# heuristic rules.
 #
 # The JSON is printed on stdout is structured as an object with 2 fields:
 #  {"library" : { artist : { album : { see_below }, ... }, ... },
-#   "playlists" : { name.m3u : { "duration": n, "encoding": e}, ... } }
+#   "playlists" : { name.m3u : { "duration": n, "encoding": e}, ... },
+#   "announcements" : { %name : { "encoding":e, "text":t, ...} }  }
 #
-# Each album is an object with fields:
+# Each album resource is an object with fields:
 #   "tracks" : array of filenames
 #   "durations" :array of track durations in seconds
 #   "encoding" : one of ogg, mp3, mp4
@@ -36,16 +48,20 @@ use strict;
 use warnings;
 use Carp;
 use Getopt::Long;
-
-# If you don't have JSON::XP, then revert to JSON::PP (in base)
-#use JSON::XP;
+use POSIX qw(strftime);
 use JSON::PP;
 
-my $Musicdir = "$ENV{'HOME'}/Music";
+# Where to find the Music library.
+# Override with environment variable MUSICDIR.
+my $Musicdir = $ENV{'MUSICDIR'} // "$ENV{'HOME'}/Music";
 
 # Where to find playlists. Default location is MPD typical.
-my $PlaylistDir = "$ENV{'HOME'}/.config/mpd/playlists";
+# Override with environment variable PLAYLISTDIR
+my $PlaylistDir = $ENV{'PLAYLISTDIR'} // "$ENV{'HOME'}/.config/mpd/playlists";
 
+print STDERR "; MUSICDIR=$Musicdir\n; PLAYLISTDIR=$PlaylistDir\n";
+
+my $ConfDir="$ENV{'HOME'}/.config/rsked";
 
 # maximum limit on artists to process
 # adjust with (--alimit=NNNN)
@@ -55,10 +71,19 @@ my $Alimit=20000;
 # turn on with  --pretty
 my $PrettyPrint=0;
 
-# Harvest playlists instead of Music library
-# enable with flag --playlists
-my $SkrapePlaylists=1;
+# It true, do not harvest playlists
+# Disable with flag --noplaylist
+my $NoPlaylist=0;
 
+# It true, do not skrape Music library
+# Disable with flag --nolibrary
+my $NoLibrary=0;
+
+# It true, do not skrape Announcements
+# Disable with flag --noannounce
+my $NoAnnounce=0;
+
+my %announcements;
 my %resources;
 my %playlists;
 
@@ -76,12 +101,19 @@ if (-x $LocalMediainfo) {
 }
 print STDERR "; Using mediainfo: $Mediainfo\n";
 
+## Vorbiscomment utility, part of vorbis utils package
+my $VorbisComment = "/usr/bin/vorbiscomment";
 
+
+# print usage information
 sub print_usage {
-    print "Usage: rskrape.pl [--pretty] [--alimit=N] [MusicDir]\n";
+    print "\nUsage: rskrape.pl [options*] [MusicDir]\n";
+    print "  [--pretty] [--alimit=N]\n";
+    print "  [--noplaylist] [--nolibrary] [--noannounce]\n\n";
     exit(0);
 }
 
+# print version of this program
 sub print_version {
     print "rskrape v1.0, perl5\n";
 }
@@ -110,6 +142,24 @@ sub track_duration_sec {
     return $msec/1000.0
 }
 
+# Return the vorbis comment [keyword=comment_text] from ogg file
+# given by the full path.  If not found, return arg3: defaultstr.
+# The utility 'vorbiscomment' may be used to set or retrieve user
+# comments in a vorbis file.  Rsked uses the "Text" comment to
+# provide a text version of the audio comment.
+#
+sub get_vorbis_comment {
+    my ($path,$keyword,$defaultstr) = @_;
+    $path =~ s/(["`])/\\$1/g;  # cleanup pathname for shell
+    my $comments = qx/$VorbisComment -l "$path"/;
+    if ( $comments=~m{ ^ $keyword = (.+) $ }xm ) {
+        return $1;
+    }
+    print STDERR "; failed to get text for: $path\n";
+    return $defaultstr;
+}
+
+#########################################################################
 
 # compile files for one album given artist directory and album name
 sub skrape_an_album {
@@ -206,6 +256,9 @@ sub abspath {
     return "$Musicdir/$fn";
 }
 
+###########################################################################
+
+
 # process one playlist
 #
 sub process_playlist {
@@ -244,7 +297,7 @@ sub process_playlist {
 # playlist scan
 #
 sub skrape_playlists {
-    opendir(my $dh, $PlaylistDir) or die "Can't open $PlaylistDir: $!";
+    opendir(my $dh, $PlaylistDir) or croak "Can't open $PlaylistDir: $!";
     while( defined(my $plname = readdir($dh)) ) {
         next unless $plname =~ /\.m3u$/;
         process_playlist( $plname );
@@ -252,10 +305,60 @@ sub skrape_playlists {
     closedir($dh);
 }
 
+
+###########################################################################
+
+# add an announcment named rname from subdirectory rdir.
+#
+sub add_announcement {
+    my ($rdir, $rname) = @_;
+    my ($fn,$ext) = $rname=~m{ (.*) \. ([^.]+) $}xm;
+    my $aname = "%" . "$fn";
+    my $enc = $ext;  # normally ogg
+    my $fullpath = "$ConfDir/$rdir/$rname";
+    my $dur = track_duration_sec( $fullpath );
+    my $text = get_vorbis_comment($fullpath,'Text',"?");
+    $announcements{$aname} = {
+        "encoding" => $enc, "duration" => $dur,
+            "location" => "$rdir/$rname", "text" => $text };
+}
+
+# announcement scan
+#
+sub skrape_announcements {
+    my $resdir = "$ConfDir/resource";
+    my $motdir = "$ConfDir/motd";
+
+    # resources
+    opendir(my $dh, $resdir) or croak "Can't open $resdir: $!";
+    while( defined(my $rname = readdir($dh)) ) {
+        next unless $rname =~ /\.ogg$/;
+        add_announcement( "resource", $rname );
+    }
+    closedir($dh);
+
+    # dynamic message of day
+    $announcements{'motd-ymd'} = {
+        "encoding" => "ogg", "duration" => 10,
+            "location" => "motd/%Y-%m-%d.ogg",
+            "dynamic" => $JSON::PP::true,
+            "text" => "(content based on calendar day/month/year)" };
+
+    $announcements{'motd-md'} = {
+        "encoding" => "ogg", "duration" => 10,
+            "location" => "motd/each-%m-%d.ogg",
+            "dynamic" => $JSON::PP::true,
+            "text" => "(content based on calendar day/month)" };
+
+}
+
 ###########################################################################
 
 GetOptions("alimit=i"=> \$Alimit,  # numeric
            "pretty" => \$PrettyPrint, # flag
+           "noplaylist" => \$NoPlaylist,
+           "nolibrary" => \$NoLibrary,
+           "noannounce" => \$NoAnnounce,
            "help" => sub { print_usage(); },
            "usage" => sub { print_usage(); },
            "version" => sub { print_version(); exit(0); }
@@ -266,8 +369,9 @@ if (scalar(@ARGV)) {
     $Musicdir = $ARGV[0];
 }
 
-skrape_library();
-skrape_playlists();
+skrape_library() unless $NoLibrary;
+skrape_playlists() unless $NoPlaylist;
+skrape_announcements() unless $NoAnnounce;
 
 my %result;
 if (scalar keys %resources) {
@@ -276,7 +380,17 @@ if (scalar keys %resources) {
 if (scalar keys %playlists) {
     $result{"playlists"} = \%playlists;
 }
+if (scalar keys %announcements) {
+    $result{"announcements"} = \%announcements;
+}
+
+my $result_version = strftime("%Y-%m-%dZ%H:%M",gmtime);
+$result{"encoding"} = "UTF-8";
+$result{ "schema"} = "1.0";
+$result{"version"} = $result_version;
 
 my $Jason = ($PrettyPrint ? JSON::PP->new->pretty([1]) : JSON::PP->new);
 my $utf8_json = $Jason->encode( \%result );
 print "$utf8_json\n";
+
+print STDERR "; wrote resources, version $result_version\n";
