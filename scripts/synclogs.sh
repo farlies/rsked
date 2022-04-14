7cb0b679065f96cf5fc3a9ac72242b48606f4f73
farlies <farlies@gmail.com>
2022-04-14 10:42:20 -0500

    also sync the config files

--- scripts/synclogs.sh
+++ scripts/synclogs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Synchronize logs to the remote host named in arg-1.
+# Synchronize logs and rsked config to the remote host named in arg-1.
 
 if [ $# != 1 ]; then
    echo "Usage: $0  remote-hostname"
@@ -12,3 +12,4 @@ RHOST=$1
 UNIT=`hostname`
 rsync -a "$HOME/logs"     logger@$RHOST:~/$UNIT
 rsync -a "$HOME/logs_old" logger@$RHOST:~/$UNIT
+rsync -a $HOME/.config/rsked/*.{json,conf}  logger@$RHOST:~/$UNIT/conf
